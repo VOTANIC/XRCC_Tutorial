d0f8f5579d3cd993b3afc55841f97ff6eb60c9d9
dannyho369 <danny.ho@votanic.com>
2025-12-05 14:49:02 +0800

    Added Release Note 3.1.4 & 3.1.4.1

---
+++ assets/js/e01c9ce5.823353b7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[7058],{28545:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/XRCC_Tutorial/update/releaseNotes/","page":1,"postsPerPage":21,"totalPages":1,"totalCount":21,"blogDescription":"Blog","blogTitle":"Release Notes"}}')}}]);
